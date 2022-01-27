71bd81a7a591e961a27fb767e14327154b5b631b
Mansur Ismailov <mansur_ismailov_1995@mail.ru>
2022-01-27 18:17:03 +0300

    add 2 stub

--- 2/Lesson2_2.java
+++ 2/Lesson2_2.java
@@ -1,11 +1,13 @@
 public class Lesson2_2 {
     public static char charExpression(int a) {
-        char simvol = '\\';
-        return (char) ( (int) simvol + a);
+        char symbol = '\\';
+        return (char) ( (int) symbol + a);
+    }
+    public static boolean isPowerOfTwo(int value) {
+        return (Integer.bitCount(Math.abs(value)) == 1);
     }
     public static void main(String[] args) {
         System.out.println(charExpression(29)); //1
-
-
+        System.out.println(isPowerOfTwo(-2));//2
     }
 }

3811a98a3098af338ee6771abd8279e7fbb6077b
Mansur Ismailov <mansur_ismailov_1995@mail.ru>
2022-03-28 02:46:19 +0300

    Решение задачи преобразования перевода строк из формата Windows в формат Unix.

---
+++ 5/Lesson5_2.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+
+public class Lesson5_2 {
+    public static String readAsString(InputStream inputStream, Charset charset) throws IOException {
+        Reader reader = new InputStreamReader(inputStream, charset);
+        StringBuilder help = new StringBuilder();
+        int count;
+        while((count = reader.read()) != -1 )
+            help.append((char)count);
+        String str = help.toString();
+        return str;
+    }
+    public static void main(String[] args) throws IOException {
+        InputStream inputStream = new ByteArrayInputStream(new byte[] {48, 49, 50, 51});
+       // Writer writer = new OutputStreamWriter(System.out, StandardCharsets.US_ASCII);
+        System.out.println(readAsString(inputStream, StandardCharsets.US_ASCII));
+
+    }
+}

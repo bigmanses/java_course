1d5eaec41cd3a827a334348a8443a5e633b73473
Mansur Ismailov <mansur_ismailov_1995@mail.ru>
2022-04-03 06:10:06 +0300

    Стримы.  Элементы вычисляются по рекуррентной формуле R(n+1) = mid(R^2), где mid = — это функция, выделяющая второй, третий и четвертый разряд переданного числа. Улучшенный код.

--- 6/Lesson6_5.java
+++ 6/Lesson6_5.java
@@ -3,17 +3,7 @@ import java.util.stream.IntStream;
 public class Lesson6_5 {
     public static IntStream pseudoRandomStream(int seed) {
 
-        return IntStream.iterate(seed, n -> { int a=0, shet = 1, i = 1, n1 = n*n;
-            while(n1 != 0 && shet <= 4){
-                   if(shet>1) {
-                       a += n1 % 10 * i;
-                       i *= 10;
-                   }
-                   shet++;
-                   n1 = n1/10;
-            }
-            return a;
-        });
+        return IntStream.iterate(seed, n -> (n*n)/10%1000);
     }
     public static void main(String[] args){
         pseudoRandomStream(13).limit(10).forEach(System.out::println);
